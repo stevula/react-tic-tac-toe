3700a8e6f68349e7650664bfef4f413445d58696
stevula <brodericksteven@gmail.com>
2018-11-26 23:45:02 -0800

    Style Square with emotion

---
+++ src/Square/Square.styles.js
@@ -0,0 +1,10 @@
+import { css } from 'emotion';
+
+export default css`
+  align-contents: center;
+  border: 1px solid black;
+  display: flex;
+  height: 1em;
+  justify-content: space-around;
+  width: 1em;
+`;
--- src/Square/Square.ui.jsx
+++ src/Square/Square.ui.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Square.styles';
 
 function Square({ mark }) {
   return (
-    <div>
+    <div className={styles}>
       {mark}
     </div>
   );
