745b76071bc7be81f5e031e250146ad93bdb500e
stevula <brodericksteven@gmail.com>
2018-11-26 22:47:32 -0800

    Add storybook and eslint

---
+++ src/Board/Board.ui.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Board(props) {
+  return (
+    <div>
+      Board
+    </div>
+  );
+}
+
+export default Board;
---
+++ src/Square/Square.ui.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Square(props) {
+  return (
+    <div>
+      Square
+    </div>
+  );
+}
+
+export default Square;
