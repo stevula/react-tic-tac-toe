3f96a3c430e520f841a68624be2b6d05d80eda60
stevula <brodericksteven@gmail.com>
2018-11-26 22:57:25 -0800

    Add dependencies

--- src/Square/Square.stories.js
+++ src/Square/Square.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import Square from './Square.ui';
 
 storiesOf('Square', module)
-  .add('with no mark', () => <Square />)
+  .add('with no mark', () => <Square />);
